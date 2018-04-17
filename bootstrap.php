c26b5b5b1983a188290fb67e5fe95fc8f6107de7
Christian Meinhard <christian.meinhard@check24.de>
2018-04-17 15:55:43 +0200

    Bootstap.php Wording

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
-function autoloader($classname)
+function autoloader($className)
 {
     $directory = ['lib', 'src'];
     foreach ($directory as $dic) {
         $file = str_replace("\\", DIRECTORY_SEPARATOR,
-            __DIR__ . DIRECTORY_SEPARATOR . $dic . DIRECTORY_SEPARATOR . $classname . '.php');
+            __DIR__ . DIRECTORY_SEPARATOR . $dic . DIRECTORY_SEPARATOR . $className . '.php');
         if (file_exists($file)) {
             break;
         }
@@ -16,4 +16,3 @@ spl_autoload_register('autoloader');
 
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
-'a';
